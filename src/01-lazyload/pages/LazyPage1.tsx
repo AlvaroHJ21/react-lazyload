8ea9094274f483c8682001d8a2a5296845da339e
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-01-14 11:34:38 -0500

    changes lazy

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -1,3 +1,7 @@
 export const LazyPage1 = () => {
     return <h1>LazyPage 1</h1>;
 };
+
+//Para trabajar con lazy load, debemos exportar el componente como default
+
+export default LazyPage1;
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,37 +1,36 @@
+import { Suspense } from 'react';
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { routes } from '../01-lazyload/routes/routes';
 import logo from '../assets/react.svg';
+
 export const Navigation = () => {
+    /**
+     * Suspense es un componente que nos permite mostrar un fallback mientras se carga el componente
+     */
     return (
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={logo} alt="React logo" />
-                    <ul>
-                        <li>
-                            <NavLink className={({ isActive }) => (isActive ? 'nav-active' : '')} to="/lazy1">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink className={({ isActive }) => (isActive ? 'nav-active' : '')} to="/lazy2">
-                                About
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink className={({ isActive }) => (isActive ? 'nav-active' : '')} to="/lazy3">
-                                Users
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/lazy1" element={<LazyPage1 />} />
-                    <Route path="/lazy2" element={<LazyPage2 />} />
-                    <Route path="/lazy3" element={<LazyPage3 />} />
-                    <Route path="/*" element={<Navigate to="/lazy1" replace />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <Suspense fallback={<span>Loading...</span>}>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={logo} alt="React logo" />
+                        <ul>
+                            {routes.map(({ to, name }, i) => (
+                                <li key={i}>
+                                    <NavLink className={({ isActive }) => (isActive ? 'nav-active' : '')} to={to}>
+                                        {name}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <Routes>
+                        {routes.map(({ path, Component }, i) => (
+                            <Route key={i} path={path} element={<Component />} />
+                        ))}
+                        <Route path="/*" element={<Navigate to={routes[0].to} />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Suspense>
     );
 };
